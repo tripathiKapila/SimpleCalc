d31c2ab9a08619dda39aa5afb4daa3339c4ad2e1
tripathiKapila <kaapiltripathi@gmail.com>
2025-02-10 10:39:09 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from calculator import add, subtract, multiply, divide
+from utils import format_result
+
+def main():
+    a, b = 10, 5
+
+    result_add = add(a, b)
+    result_subtract = subtract(a, b)
+    result_multiply = multiply(a, b)
+    result_divide = divide(a, b)
+
+    print(format_result(result_add))
+    print(format_result(result_subtract))
+    print(format_result(result_multiply))
+    print(format_result(result_divide))
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,2 @@
+def format_result(result):
+    return f"The result is: {result}"
